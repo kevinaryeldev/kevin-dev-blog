f4348694a2e4e5b9ede8fef5adc2ce5dbbaebfb2
Kevin <kevinaryel.dev@gmail.com>
2022-06-07 02:07:42 -0300

    style(global): Theme Created

---
+++ src/style/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,
+}
+const theme = extendTheme({
+  config,
+  colors: {
+    primary: '#151525',
+    lilight: '#9090FF',
+    secondary: '#304256',
+    terciary: '#345d6b',
+    alert: '#A20623',
+    positive: '#419D78',
+  },
+})
+
+export default theme
